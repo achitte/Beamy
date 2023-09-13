355fbb371fbf8cbe8fab44b041332b7d89ad0d41
Amol Chitte <amol.chitte@tatatechnologies.com.com>
2023-09-13 16:08:25 -0700

    Beamy

---
+++ dotnet/src/SemanticKernel.Abstractions/Text/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.SemanticKernel.Text;
+
+internal static class StringExtensions
+{
+    internal static string NormalizeLineEndings(this string src)
+    {
+        return src.Replace("\r\n", "\n");
+    }
+
+    internal static string[] SplitEx(this string src, char separator, StringSplitOptions options = StringSplitOptions.None)
+    {
+        return src.Split(new[] { separator }, options);
+    }
+
+    internal static string[] SplitEx(this string src, string separator, StringSplitOptions options = StringSplitOptions.None)
+    {
+        return src.Split(new[] { separator }, options);
+    }
+
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? data)
+    {
+        return string.IsNullOrEmpty(data);
+    }
+
+    public static bool IsNullOrWhitespace([NotNullWhen(false)] this string? data)
+    {
+        return string.IsNullOrWhiteSpace(data);
+    }
+}
